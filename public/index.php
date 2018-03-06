77c8ff5b89074a7018a3257f7cc3d1ab6d5709c3
Sebastian Guzman <sebastiangabriel.guzman@gmail.com>
2018-03-06 15:42:59 -0300

    cambiando a mvc

--- public/index.php
+++ public/index.php
@@ -6,16 +6,39 @@ error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
 include_once '../config.php';
 
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . $baseDir;
+define('BASE_URL', $baseUrl);
+
+var_dump($baseDir);
 $route = $_GET['route'] ?? '/';
 
-switch ($route) {
-  case '/':
-    require '../index.php';
-    break;
-  case '/admin':
-    require '../admin/index.php';
-    break;
-  case '/admin/posts';
-    require '../admin/posts.php';
-    break;
+function render($fileName, $params = []){
+  ob_start();
+  extract($params);
+  include $fileName;
+  return ob_get_clean();
 }
+use Phroute\Phroute\RouteCollector;
+
+$router = new RouteCollector();
+$router->get('admin', function(){
+    //si no paso nada enel segundo parametro se usa el parametro default que es el que aparece enla funcion render
+    return render('../views/admin/index.php');
+});
+
+$router->get('/',function() use ($pdo){
+  $query = $pdo -> prepare('SELECT * FROM blog_posts ORDER BY id DESC');
+  $query -> execute();
+
+  $blogPosts = $query -> fetchAll(PDO::FETCH_ASSOC);
+  return render('',['blog']);
+  include '../views/index.php';
+  return 'Route /';
+});
+
+$dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+
+
+echo $response;
